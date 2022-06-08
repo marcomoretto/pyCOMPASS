95fd81065bb9ed1b3dc10a9a2f82a8958d1128a6
marco.moretto@fmach.it <marco.moretto@fmach.it>
2022-06-08 13:55:25 +0200

    Annotation

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 import setuptools
-import pycompass
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="pyCOMPASS",
-    version=pycompass.__version__,
+    version="2.0.1",
     author="Marco Moretto",
     author_email="marco.moretto@fmach.it",
     description="A Python interface to COMPASS, the gene expression compendia GraphQL endpoint",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="",
-    packages=setuptools.find_packages('pycompass'),
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
